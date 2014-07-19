52eb88b0f5d9a711287ef27c3df42da81e177f6d
Matt Ludwigs <matt@mattludwigs.com>
2014-07-19 16:39:41 -0700

    update: dev

--- data/siteData.js
+++ data/siteData.js
@@ -4,8 +4,6 @@ module.exports = (function() {
     title: "Acitve Angency",
     heading: "Future Site of Active Agency",
     nav: ['About', 'People', 'Work', 'Contact'],
-    people: ['Justin', 'Matt', 'Ashur'],
-    bios: [justin_bio, matt_bio, ashur_bio],
     env: 'dev',
     page: true
   }
